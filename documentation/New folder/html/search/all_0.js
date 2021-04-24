f046a5f560a3f9ef523d10b21ed0b465b4072610
Mallineni-Venkata-Meghana <80767884+Mallineni-Venkata-Meghana@users.noreply.github.com>
2021-04-24 15:44:11 +0530

    Delete documentation/New folder/html/search directory

--- documentation/New folder/html/search/all_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['fuse_2ec_0',['fuse.c',['../fuse_8c.html',1,'']]],
-  ['fuses_1',['FUSES',['../fuse_8c.html#a9eca7681a8cf33b72c057cbd699dc9e6',1,'fuse.c']]]
-];
--- documentation/New folder/html/search/variables_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['fuses_7',['FUSES',['../fuse_8c.html#a9eca7681a8cf33b72c057cbd699dc9e6',1,'fuse.c']]]
-];
